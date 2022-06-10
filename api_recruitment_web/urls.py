e2ecdb017224cb3ec3c5a99bf433e4a5c655336b
godChoi96 <oinkoink71@naver.com>
2022-06-11 08:00:11 +0900

    2022.06.11 - Convenient Click

--- api_recruitment_web/urls.py
+++ api_recruitment_web/urls.py
@@ -17,12 +17,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # api
     path('api/', include('api.urls')),
-]
 
+    # app
+    path('', views.IndexView.as_view(), name="index"),
+]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
